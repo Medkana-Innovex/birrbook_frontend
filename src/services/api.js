1da69eaa1844df62860f726342968ddc4330ccaa
Eyob Mekonnen <emekonnen35@gmail.com>
2026-06-05 22:31:05 +0300

    use VITE_API_URL env var for api base url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:7001',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:7001',
 });
 
 api.interceptors.request.use((config) => {
